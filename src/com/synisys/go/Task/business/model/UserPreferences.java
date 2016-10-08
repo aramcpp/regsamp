93f62dd831fe2cd0019abc728c5fe681d0259287
Aram Hovhannisyan <aramcpp@gmail.com>
2016-10-08 18:21:01 +0400

    new interfaces

---
+++ src/com/synisys/go/Task/business/model/UserPreferences.java
@@ -0,0 +1,7 @@
+package com.synisys.go.Task.business.model;
+
+/**
+ * Created by aram.hovhannisyan on 10/8/2016.
+ */
+public class UserPreferences {
+}
---
+++ src/com/synisys/go/Task/business/model/impl/UserImpl.java
@@ -0,0 +1,7 @@
+package com.synisys.go.Task.business.model.impl;
+
+/**
+ * Created by aram.hovhannisyan on 10/8/2016.
+ */
+public class UserImpl {
+}
